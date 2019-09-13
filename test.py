3374ecd1326292dfdd82197ab04ce190fdb60768
parupappa <fuzino29@gmail.com>
2019-09-13 11:33:06 +0900

    LED点灯

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(14,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
 
 flag = 1
 while True:
-    GPIO.output(14,flag)
+    GPIO.output(15,flag)
     flag = flag ^ 1
     time.sleep(1)
