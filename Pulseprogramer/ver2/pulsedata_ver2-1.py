322f69b9a7b4cae9a1b22ed0e2436b83ad14ee65
横尾杏之介 <yokooannosuke@yokooannosukenoMacBook-Air.local>
2020-05-23 11:51:58 +0900

    DDS追加

--- Pulseprogramer/ver2/pulsedata_ver2-1.py
+++ Pulseprogramer/ver2/pulsedata_ver2-1.py
@@ -117,10 +117,10 @@ DDSnamelist = [DDS1, DDS2]
 col_nvalue = []
 DDS_scset = []
 DDS_nscset = []
-DDS_40bitset = []
-DDS_16bitset = []
+DDS_40set = []
 
-for n in range(24, 29):  # 右の値29,37を指定すれば繰り返し回数、つまりDDSの個数を指定出来る
+
+for n in range(24, 37):  # 右の値29,37を指定すれば繰り返し回数、つまりDDSの個数を指定出来る
     if n % 8 == 0:  # 24(Y),32(AG)行目 #8にしないと28（AC）が含まれちゃう
         col_value = sheet.col_values(int(n))
         del col_value[0:3]
@@ -132,40 +132,53 @@ for n in range(24, 29):  # 右の値29,37を指定すれば繰り返し回数、
             DDS_scset.append(b)
             DDS_nscset.append(DDS_scset)
             DDS_scset = []
-        print(DDS_nscset)  # RFのsc部分のデータ [[RF1,0],...[RF3,12]]
+        # print(DDS_nscset)  # RFのsc部分のデータ [[RF1,0],...[RF3,12]]
 
-    elif (n - 2) % 8 == 4:  # 40bitdata 28(AC),36(AK)行目
+    elif n % 8 == 4:  # 40bitdata 28(AC),36(AK)行目
         col_value = sheet.col_values(int(n))
         del col_value[0:3]
         col_nvalue = [s for s in col_value if s != '']  # col_value内の’’を削除
         for m in range(len(col_nvalue)):
-            40bitdata = col_nvalue[m]
-            d = int(c)
-            AD_ecset.append('')  # ecの方は何も入れない
-            AD_ecset.append(d)
-            AD_necset.append(AD_ecset)
-            AD_ecset = []
-        # print(AD_necset)  # RFのec部分のデータ　[['',2],...['',18]]
-
-allADdatalist = AD_nscset + AD_necset
-# print(allADdatalist)  # [['AD1', 10], ['', 1], ... ]
+            bitdata_40 = col_nvalue[m]
+            DDS_40set.append(bitdata_40)
+        print(DDS_40set)  # DDSの40bit部分のデータ
 
 
-######################################################################
+# 40bitのDDSdataを16進数10bitに変換
 
-# RF,ADの統合を行う
-"""
-allRFADdatalist = allRFdatalist + allADdatalist
-allRFADdatalist.sort(key=lambda count: count[1])  # count順になるようにsort
-# print(allRFADdatalist) # [['RF1', 0], ['', 2], ['RF1', 5], ['', 9], ['AD1', 10], ['', 11]]
+hexdata = ''
+DDSdata_hex = []  # 16進数10bitのDDSdata
+n = 0
+for m in range(len(DDS_40set)):
+    str = DDS_40set[m]
+    hexdata = ''
+    n = 0
+    while n + 3 < 40:
+        four_str = str[n:n + 4]
+        four_int = int("0b"+four_str, 0)  # 2進数four_strを数値に変換
+        hex_str = format(four_int, 'x')  # 数値four_intを16進数に変換
+        hexdata = hexdata + hex_str
+        n = n + 4
+    DDSdata_hex.append(hexdata)  # 4bitずつ16進数に変換
+print(DDSdata_hex)  # 40bitのDDS_40setを10bitの16進数に変換
 
 
+####################################################################################################
+
+# RF,AD,DDSの統合を行う
+
+allRFADDDSdatalist = allRFdatalist + allADdatalist + DDS_nscset
+allRFADDDSdatalist.sort(key=lambda count: count[1])  # count順になるようにsort
+# print(allRFADDDSdatalist) # [['RF1', 0], ['', 2]....['DDS1', 20], ['DDS2', 20], ['DDS1', 24], ['DDS2', 80]]
+
+
+# 対象とカウント値に分割
 targetlist0 = []
 countlist0 = []
 
-for n in range(len(allRFADdatalist)):  # 対象とカウント値に分割
-    targetlist0.append(allRFADdatalist[n][0])
-    countlist0.append(allRFADdatalist[n][1])
-print(targetlist0)  # ['RF1', '', 'RF1', '', 'AD1', '']
-print(countlist0)  # [0, 2, 5, 9, 10, 11]
-"""
+for n in range(len(allRFADDDSdatalist)):
+    targetlist0.append(allRFADDDSdatalist[n][0])
+    countlist0.append(allRFADDDSdatalist[n][1])
+
+# print(targetlist0)  # ['RF1', '', 'RF1', '', 'AD1', '']
+# print(countlist0)  # [0, 2, 5, 9, 10, 11]
