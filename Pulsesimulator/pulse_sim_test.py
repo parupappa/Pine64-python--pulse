63f219a60dc8cf5a55ae302af1aa1865f6fe6b68
横尾杏之介 <yokoo.an209@gmail.com>
2020-07-06 15:15:50 +0900

    テスト

--- Pulsesimulator/pulse_sim_test.py
+++ Pulsesimulator/pulse_sim_test.py
@@ -47,7 +47,7 @@ print(bc1_int)
 
 counter = 0
 while counter < len(sc1_int):
-    if sc1_int[counter] == counter:
+    if counter == sc1_int[counter]:
         GPIO.output(PL[0], 1)
         sleep(bc1_int[counter])
         print(counter)
