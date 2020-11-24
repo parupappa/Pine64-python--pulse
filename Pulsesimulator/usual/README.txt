129c4273fb0eb3e738e36951c234dbe1c9dd710c
横尾杏之介 <yokoo.an209@gmail.com>
2020-11-24 10:34:28 +0900

    create README document

---
+++ Pulsesimulator/usual/README.txt
@@ -0,0 +1,1 @@
+https://docs.google.com/document/d/1Pfq7P0z5TeqWNOfaQH-KXd28RE7cd3wU4f8Hiyk-QVI/edit?usp=sharing
--- Pulsesimulator/usual/pulsesim1.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-fname_Dar = '/Users/yokooannosuke/Cording/Pine64-python--pulse/Pulsesimulator/usual/pulse_simdata.csv'
-csvfile = open(fname_Dar)
-
-a = []
-for row in csv.reader(csvfile):
-    a.append(row[1])  # 取得したい列番号を指定（0始まり）
-print(a)
